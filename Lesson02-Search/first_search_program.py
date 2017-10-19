0d1cf1c5af445bceb5ae48abb93e4d2a336c0912
Cedric Bodet <bodetc@gmail.com>
2017-10-19 17:52:37 +0200

    Added path to search

--- Lesson02-Search/first_search_program.py
+++ Lesson02-Search/first_search_program.py
@@ -32,79 +32,51 @@ delta = [[-1, 0],  # go up
 delta_name = ['^', '<', 'v', '>']
 
 
-def search(grid, init, goal, cost, debug = False):
+def search(grid, init, goal, cost, debug=False):
     def getTriplet(extension, position):
         return [extension, position[0], position[1]]
 
     def unpack(triplet):
         return triplet[0], [triplet[1], triplet[2]]
 
-    def popNextValue(open):
-        open.sort()
-        open.reverse()
-        return open.pop()
-
     def getNeighbours(pos):
         neighbours = []
-        for d in delta:
+        arrows = []
+        for d, arrow in zip(delta, delta_name):
             neighbours.append([pos[0] + d[0], pos[1] + d[1]])
-        return neighbours
+            arrows.append(arrow)
+        return neighbours, arrows
 
     def isSamePosition(pos1, pos2):
         return pos1[0] == pos2[0] and pos1[1] == pos2[1]
-
-    def isInGrid(pos, grid):
-        return 0 <= pos[0] < len(grid) and 0 <= pos[1] < len(grid[0])
-
-    def isOpen(pos, closed):
-        return closed[pos[0]][pos[1]] == 0
 
     expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
 
     closed = [[grid[row][col] for col in range(len(grid[0]))] for row in range(len(grid))]
     open = [getTriplet(0, init)]
 
-    iteration = 0
+    previous = [[[] for row in range(len(grid[0]))] for col in range(len(grid))]
+    all_arrows = [['X' for row in range(len(grid[0]))] for col in range(len(grid))]
 
-    if debug:
-        print 'Initial open list:'
-        for o in open:
-            print '    ', o
-        print '---'
+    def popNextValue():
+        open.sort()
+        open.reverse()
+        return open.pop()
 
-    while True:
+    def isInGrid(pos):
+        return 0 <= pos[0] < len(grid) and 0 <= pos[1] < len(grid[0])
 
-        # If open nodes are empty, it is impossible to find a solution
-        if len(open) == 0:
-            print 'fail'
-            return expand
+    def isOpen(pos):
+        return closed[pos[0]][pos[1]] == 0
 
-        next = popNextValue(open)
-        g, pos = unpack(next)
-
-        # Close position
-        closed[pos[0]][pos[1]] = 1
-
-        # Write expansion
-        expand[pos[0]][pos[1]] = iteration
-        iteration += 1
-
-        if debug:
-            print 'Next item:'
-            print '    ', next
-
-        if isSamePosition(pos, goal):
-            if debug:
-                print 'Search successful!'
-            return expand
-
-        neighbours = getNeighbours(pos)
-        for pos2 in neighbours:
+    def appendNeigbours(g, pos):
+        neighbours, arrows = getNeighbours(pos)
+        for pos2, arrow in zip(neighbours, arrows):
             if debug:
                 print 'Searching ', pos2
 
-            if isInGrid(pos2, grid) and isOpen(pos2, closed):
-                g2 = g+cost
+            if isInGrid(pos2) and isOpen(pos2):
+                g2 = g + cost
                 new = getTriplet(g2, pos2)
                 if debug:
                     print 'Append list item:'
@@ -114,13 +86,76 @@ def search(grid, init, goal, cost, debug = False):
                 # Close position
                 closed[pos2[0]][pos2[1]] = 1
 
+                # Fill helpers
+                all_arrows[pos2[0]][pos2[1]] = arrow
+                previous[pos2[0]][pos2[1]] = pos
         if debug:
             print 'New open list:'
             for o in open:
                 print '    ', o
             print '---'
 
-expand = search(grid, init, goal, cost, debug=True)
+    iteration = 0
 
-print 'Expansion:'
-print expand
+    if debug:
+        print 'Initial open list:'
+        for o in open:
+            print '    ', o
+        print '---'
+
+    fail = False
+    found = False
+
+    while not fail and not found:
+
+        # If open nodes are empty, it is impossible to find a solution
+        if len(open) == 0:
+            print 'fail'
+            fail = True
+
+        else:
+            next = popNextValue()
+            g, pos = unpack(next)
+
+            # Close position
+            closed[pos[0]][pos[1]] = 1
+
+            # Write expansion
+            expand[pos[0]][pos[1]] = iteration
+            iteration += 1
+
+            if debug:
+                print 'Next item:'
+                print '    ', next
+
+            if isSamePosition(pos, goal):
+                if debug:
+                    print 'Search successful!'
+                found = True
+
+            else:
+                appendNeigbours(g, pos)
+
+    if debug:
+        print 'Expansion:'
+        print expand
+        print 'Arrows:'
+        print all_arrows
+
+    if found:
+        path=[[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
+        current = goal
+        path[goal[0]][goal[1]]='*'
+
+        while not isSamePosition(current, init):
+            p=previous[current[0]][current[1]]
+            path[p[0]][p[1]]=all_arrows[current[0]][current[1]]
+            current=p
+
+        return path
+
+
+path = search(grid, init, goal, cost, debug=True)
+
+print 'Path:'
+print path
